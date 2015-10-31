cef19fad45d84b11eaed52ce32e9fa33b01a4456
yinjimmy <jimmy.yin5@gmail.com>
2015-10-31 11:44:24 +0800

    hello world

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,106 @@
+#include "HelloWorldScene.h"
+#include "cocostudio/CocoStudio.h"
+#include "ui/CocosGUI.h"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+
+USING_NS_CC;
+
+using namespace cocostudio::timeline;
+
+static GLFWwindow* window = nullptr;
+static bool show_test_window = true;
+static bool show_another_window = false;
+static ImVec4 clear_color = ImColor(114, 144, 154);
+
+Scene* HelloWorld::createScene()
+{
+    // 'scene' is an autorelease object
+    auto scene = Scene::create();
+    
+    // 'layer' is an autorelease object
+    auto layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !Layer::init() )
+    {
+        return false;
+    }
+    
+    auto rootNode = CSLoader::createNode("MainScene.csb");
+
+    addChild(rootNode);
+
+    window = ((GLViewImpl*)Director::getInstance()->getOpenGLView())->getWindow();
+    ImGui_ImplGlfw_Init(window, true);
+    
+    setGLProgram(GLProgramCache::getInstance()->getGLProgram(GLProgram::SHADER_NAME_POSITION_COLOR));
+    
+    return true;
+}
+
+void HelloWorld::visit(cocos2d::Renderer *renderer, const cocos2d::Mat4 &parentTransform, uint32_t parentFlags)
+{
+    Layer::visit(renderer, parentTransform, parentFlags);
+    _command.init(_globalZOrder);
+    _command.func = CC_CALLBACK_0(HelloWorld::onDraw, this);
+    Director::getInstance()->getRenderer()->addCommand(&_command);
+}
+
+void HelloWorld::onDraw()
+{
+    glUseProgram(0);
+    
+    if (window) {
+        ImGui_ImplGlfw_NewFrame();
+        
+        // 1. Show a simple window
+        // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
+        {
+            static float f = 0.0f;
+            ImGui::Text("Hello, world!");
+            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            if (ImGui::Button("Test Window")) show_test_window ^= 1;
+            if (ImGui::Button("Another Window")) show_another_window ^= 1;
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+        }
+        
+        // 2. Show another simple window, this time using an explicit Begin/End pair
+        if (show_another_window)
+        {
+            ImGui::SetNextWindowSize(ImVec2(200,100), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("Another Window", &show_another_window);
+            ImGui::Text("Hello");
+            ImGui::End();
+        }
+        
+        // 3. Show the ImGui test window. Most of the sample code is in ImGui::ShowTestWindow()
+        if (show_test_window)
+        {
+            ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+            ImGui::ShowTestWindow(&show_test_window);
+        }
+        
+        // Rendering
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        //        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        //        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui::Render();
+    }
+    
+}
