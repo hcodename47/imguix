c525e4f0c3b3ca1b3acf27f3e1be1e0a5d4b53a4
local <local@host>
2019-06-10 22:24:18 +0800

    add spine for test

--- Classes/HelloWorldScene.h
+++ Classes/HelloWorldScene.h
@@ -2,6 +2,7 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include <spine/spine-cocos2dx.h>
 
 class HelloWorld : public cocos2d::Layer
 {
@@ -9,6 +10,8 @@ public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
 
+    ~HelloWorld();
+    
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
 
@@ -17,6 +20,13 @@ public:
 
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
+    
+    void createSpineTest();
+protected:
+    spAtlas* _atlas = nullptr;
+    spAttachmentLoader* _attachmentLoader = nullptr;
+    spSkeletonData* _skeletonData = nullptr;
+    spAnimationStateData* _stateData = nullptr;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
