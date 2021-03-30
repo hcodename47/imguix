d7556fc3ff5ad0c7bd3a74b01631b1903bbd752b
localhost <local@host>
2021-03-30 13:41:16 +0800

    refactor: use imgui_impl_glfw.cpp & imgui_impl_opengl2.cpp as default BackendPlatformName & BackendRendererName
    codes from https://github.com/ocornut/imgui/tree/master/backends

--- Classes/ImGuiExt/CCImGuiLayer.cpp
+++ Classes/ImGuiExt/CCImGuiLayer.cpp
@@ -1,9 +1,26 @@
 #include "CCImGuiLayer.h"
 #include "imgui/imgui.h"
-#include "imgui_impl_cocos2dx.h"
+//#include "imgui_impl_cocos2dx.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl2.h"
 #include "CCIMGUI.h"
 
 USING_NS_CC;
+
+static GLFWwindow* getGLFWWindow()
+{
+    GLViewImpl* glview = (GLViewImpl*)Director::getInstance()->getOpenGLView();
+    GLFWwindow* window = glview->getWindow();
+    return window;
+}
+
+ImGuiLayer::~ImGuiLayer()
+{
+    // Cleanup
+    ImGui_ImplOpenGL2_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+}
 
 void ImGuiLayer::createAndKeepOnTop()
 {
@@ -50,6 +67,33 @@ bool ImGuiLayer::init()
         return false;
     }
 	
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+    //io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsClassic();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(getGLFWWindow(), true);
+    ImGui_ImplOpenGL2_Init();
+
 #if COCOS2D_VERSION < 0x00040000
     // init imgui
     setGLProgram(GLProgramCache::getInstance()->getGLProgram(GLProgram::SHADER_NAME_POSITION_COLOR));
@@ -75,24 +119,46 @@ void ImGuiLayer::visit(cocos2d::Renderer *renderer, const cocos2d::Mat4 &parentT
     Director::getInstance()->getRenderer()->addCommand(&_command);
 }
 
+// main loop from https://github.com/ocornut/imgui/blob/master/examples/example_glfw_opengl2/main.cpp#L79
 void ImGuiLayer::onDraw()
 {
-#if COCOS2D_VERSION < 0x00040000
-    getGLProgram()->use();
-#endif
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL2_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
 
-    // create frame
-    ImGui_ImplCocos2dx_NewFrame();
-    
-    // draw all gui
+
+    // Draw All custom ImGui Widgets
     CCIMGUI::getInstance()->updateImGUI();
-    
+
+    // Rendering
+    ImGui::Render();
+
+    auto window = getGLFWWindow();
+
 #if COCOS2D_VERSION < 0x00040000
-    // rendering
+    // If you are using this code with non-legacy OpenGL header/contexts (which you should not, prefer using imgui_impl_opengl3.cpp!!),
+    // you may need to backup/reset/restore other state, e.g. for current shader using the commented lines below.
+    GLint last_program;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &last_program);
     glUseProgram(0);
 #endif
-    
-    ImGui::Render();
-    
-    ImGui_ImplCocos2dx_RenderDrawData(ImGui::GetDrawData());
+
+    ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData());
+
+#if COCOS2D_VERSION < 0x00040000
+    glUseProgram(last_program);
+#endif
+
+    // Update and Render additional Platform Windows
+    // (Platform functions may change the current OpenGL context, so we save/restore it to make it easier to paste this code elsewhere.
+    //  For this specific demo app we could also call glfwMakeContextCurrent(window) directly)
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        GLFWwindow* backup_current_context = glfwGetCurrentContext();
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+        glfwMakeContextCurrent(backup_current_context);
+    }
 }
